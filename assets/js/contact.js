6cd815176b9a2d7cf35cf02c3569772650a648b0
Anjali Shrivastava <anjalishri12342@gmail.com>
2023-07-12 12:56:25 +0530

    added email logic

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -11,8 +11,8 @@ contactForm.querySelectorAll('.form-control').forEach( (inputElement) => {
 function sendContactData(e) {
     e.preventDefault();
     Email.send({
-        SecureToken: "7efd23a8-c35a-4cfc-9f5a-c6dcc3852e02",
-        To: 'anjalishri12342@gmail.com',
+        SecureToken: "91fe88cb-7459-4567-b1c9-2dd65d613a86",
+        To: "anjalishri12342@gmail.com",
         From: "Info@klhcuttingsolutions.com",
         Subject: "Enquiry for KLH cutting industry.",
         Body: "Test email"
