f66a1c8d39f49b21b798f87b81cc7702ee36bb22
Anjali Shrivastava <anjalishri12342@gmail.com>
2023-07-12 13:06:29 +0530

    mail

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,7 +1,7 @@
 const contactForm = document.getElementById('contactForm');
 
 let formData = {}
-contactForm.querySelectorAll('.form-control').forEach( (inputElement) => {
+contactForm.querySelectorAll('.form-control').forEach((inputElement) => {
     if (inputElement.name) {
         formData[inputElement.name] = formData[inputElement.value];
     }
@@ -11,12 +11,24 @@ contactForm.querySelectorAll('.form-control').forEach( (inputElement) => {
 function sendContactData(e) {
     e.preventDefault();
     Email.send({
-        SecureToken: "91fe88cb-7459-4567-b1c9-2dd65d613a86",
-        To: "anjalishri12342@gmail.com",
-        From: "Info@klhcuttingsolutions.com",
+        Host: "smtp.elasticemail.com",
+        Port: "2525",
+        Username: "Info@klhcuttingsolutions.com",
+        Password: "FA8ADBEA7245FCA6B8681D9ADA07E9998B2E",
+        To: 'anjalishri12342@gmail.com',
+        From: "Info@klhcuttingsolutions.co",
         Subject: "Enquiry for KLH cutting industry.",
         Body: "Test email"
     }).then(
         message => alert("Your message has been received! We'll get in touch soon")
-    )
+    );
+    // Email.send({
+    //     SecureToken: "91fe88cb-7459-4567-b1c9-2dd65d613a86",
+    //     To: "anjalishri12342@gmail.com",
+    //     From: "Info@klhcuttingsolutions.com",
+    //     Subject: "Enquiry for KLH cutting industry.",
+    //     Body: "Test email"
+    // }).then(
+    //     message => alert("Your message has been received! We'll get in touch soon")
+    // )
 }
